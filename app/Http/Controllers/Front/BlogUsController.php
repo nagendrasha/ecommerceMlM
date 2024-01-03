c18d402e6d9b515ae7ee120e5b1657131075ff84
rahulchugh1001 <rahul@maxmaven-solutions.com>
2024-01-03 22:32:05 +0530

    example env

--- app/Http/Controllers/Front/BlogUsController.php
+++ app/Http/Controllers/Front/BlogUsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use DB;
+
 class BlogUsController extends Controller
 {
     public function Index()
