a5c57ff70bd3c4af8191c22552fe3bc0672cca73
vishalsaroch <vishalsaroch1995@gmail.com>
2024-01-03 23:29:45 +0530

    faq controller and views code update

--- app/Http/Controllers/Front/FAQController.php
+++ app/Http/Controllers/Front/FAQController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use DB;
 
 class FAQController extends Controller
 {
     public function Index()
     {
-
-        return view('Front.faq');
+        $data['faqs'] = DB::table('faqs')->where(['status'=>1,'deleted'=>0])->orderBy('sort','ASC')->get();
+        return view('Front.faq', $data);
     }
 }
