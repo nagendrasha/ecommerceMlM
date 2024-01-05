af0a7ee5a103ee30befd411296dead2917f16b34
vishalsaroch <vishalsaroch1995@gmail.com>
2024-01-05 21:25:42 +0530

    index controller slider data get

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -11,6 +11,8 @@ class IndexController extends Controller
 
     public function Index()
     {
+        $data['sliders'] = DB::table('sliders')->where(['status'=>1, 'deleted'=>0])->orderBy('sort','ASC')->get();
+        
         $data['col_four'] = DB::table('banners')->where(['status'=>1 ,'category'=>4 ,'deleted'=>0])->orderBy('sort','ASC')->get();
         $data['col_twelve'] = DB::table('banners')->where(['status'=>1 ,'category'=>12 ,'deleted'=>0])->orderBy('sort','ASC')->get();
         $data['col_six'] = DB::table('banners')->where(['status'=>1 ,'category'=>12 ,'deleted'=>0])->orderBy('sort','ASC')->get();
