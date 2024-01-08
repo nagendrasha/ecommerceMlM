198e05baa417cb320d6f1b0e3da53753f308145c
SakirSaifi1998 <sakirsaificlub@gmail.com>
2024-01-08 22:35:44 +0530

    mobile menu set

--- resources/views/Front/layout/header.blade.php
+++ resources/views/Front/layout/header.blade.php
@@ -169,7 +169,7 @@
                            </a>
                            <!-- Header Cart End -->
                            <!-- Header menu Start -->
-                           <a href="#ec-mobile-menu" class="ec-header-btn ec-side-toggle d-lg-none">
+                           <a href="#ec-mobile-menu" class="ec-header-btn d-lg-none">
                                <i class="ecicon eci-bars"></i>
                            </a>
                            <!-- Header menu End -->
@@ -452,7 +452,28 @@
     display:block;
 }
    </style>
-   <div class="ec-side-cart-overlay"></div>
+   <div class="ec-side-cart-overlays">
+    <div class="cross-fade"><i class="fa-solid fa-circle-xmark"></i></div>
+    <div class="mobile-header-logo">
+        <a href="{{url('/')}}"><img src="{{asset('Front/assets/images/logo/logo.png')}}" alt="Site Logo"/><img
+                class="dark-logo" src="{{asset('Front/assets/images/logo/logo.png')}}" alt="Site Logo"
+                style="display: none;" /></a>
+    </div>
+    <ul class="navs">
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">faqs</a></li>
+        <li><a href="">Track Order</a></li>
+        <li><a href="">Blogs</a></li>
+        <li><a href="">Contact</a></li>
+    </ul>
+    <div class="ec-footer-widget ec-share mt-5">
+        <ul class="mobile-flex">
+            <li class="ec-share-link"><a href="#"><img src="http://localhost/ecommerceMLM/public/Front/assets/images/icons/iphone.png" alt=""></a></li>
+            <li class="ec-share-link"><a href="#"><img src="http://localhost/ecommerceMLM/public/Front/assets/images/icons/google.png" alt=""></a></li>
+        </ul>
+    </div>
+   </div>
    <div id="ec-side-cart" class="ec-side-cart">
        <div class="ec-cart-inner">
            <div class="ec-cart-top">
